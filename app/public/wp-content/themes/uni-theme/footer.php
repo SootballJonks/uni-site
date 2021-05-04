8cefd55624ae864b89856b9bc4b63a98d8d3a304
Jonks <anastasiablackhold@gmail.com>
2021-05-04 12:57:07 -0700

    Admin minibar added.

---
+++ app/public/wp-content/themes/uni-theme/footer.php
@@ -0,0 +1,5 @@
+<h5>Footer!!</h5>
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- app/public/wp-content/themes/uni-theme/index.php
+++ app/public/wp-content/themes/uni-theme/index.php
@@ -1,23 +1,12 @@
-<!-- <?php
-  //Some examples...
-  $names = array("Celaena", "Rowan", "Chaol", "Dorian"); //php array
-  
-  $count = 0; //php variable
-
-  while($count < count($names)) {
-    echo "<li>Hi, my name is $names[$count].</li>";
-    $count++;
-  }
-?>
-
-<h1><?php bloginfo('name'); ?></h1>
-<p><?php bloginfo('description'); ?></p> -->
-
 <?php 
+  get_header(); 
+
   while(have_posts()) {
     the_post(); ?>
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php the_content(); ?>
     <hr>
   <?php }
+
+  get_footer();
 ?>

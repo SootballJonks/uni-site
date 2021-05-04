616076d4960985fc62787d3c1aae5462335d04fc
Jonks <anastasiablackhold@gmail.com>
2021-05-04 10:31:26 -0700

    PHP examples added to new theme

--- app/public/wp-content/themes/uni-theme/index.php
+++ app/public/wp-content/themes/uni-theme/index.php
@@ -1,1 +1,15 @@
-placeholder text inside index.php!
+<?php
+  //Some examples...
+  $names = array("Celaena", "Rowan", "Chaol", "Dorian");
+  
+  $count = 0;
+
+  while($count < count($names)) {
+    echo "<li>Hi, my name is $names[$count].</li>";
+    $count++;
+  }
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
+
